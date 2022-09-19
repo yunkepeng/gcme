231abe9b0fc52a6ae80114b80d3d3e119efde9e6
yunkepeng <yunke.peng@usys.ethz.ch>
2022-09-19 17:13:12 +0200

    combination

--- submission/table_combination.R
+++ submission/table_combination.R
@@ -23,6 +23,12 @@ kevin_othervars <- subset(kevin_othervars,response=="vcmax")
 siteinfo <- unique(kevin_othervars[,c("site","exp","co2_a","co2_e")])
 plot1 <- merge(plot_output2,siteinfo,by=c("exp"),all.x=TRUE)
 
+# 4 points added - perhaps because there are more than 1 co2_a and co2_e
+unique(plot1[,c("site","co2_a","co2_e")])
+
+#they are soyface and riceface_japan_l_3938_14057
+
+
 #merge with co2_a, co2_e, site-name in Smith
 smith_co2 <- read.csv("~/data/smith_keenan_gcb/gcb_co2/co2_data.csv")
 smith_co2$exp_nam <- smith_co2$SiteID
@@ -73,24 +79,6 @@ vcmax25_warmingco2_siteinfo <- rename(smith_co2_final, c(ambient = ambient_vcmax
                                                          co2_a=ambient_co2, co2_e=Ca,
                                                          ambient_j = ambient_jmax, elevated_j =Jmax, ambient_Se_j=ambient_jmaxSE, elevated_Se_j=Jmax_SE))
 
-
-vcmax25_warmingco2_siteinfo$ecosystem[vcmax25_warmingco2_siteinfo$Stature=="Tree"] <- "forest"
-vcmax25_warmingco2_siteinfo$ecosystem[vcmax25_warmingco2_siteinfo$SiteID=="ChinaRiceFACE"] <- "cropland"
-vcmax25_warmingco2_siteinfo$ecosystem[vcmax25_warmingco2_siteinfo$SiteID=="UAMACFACE"] <- "cropland"
-vcmax25_warmingco2_siteinfo$ecosystem[vcmax25_warmingco2_siteinfo$SiteID=="GlycineCE"] <- "cropland"
-vcmax25_warmingco2_siteinfo$ecosystem[vcmax25_warmingco2_siteinfo$SiteID=="GossypiumGC"] <- "cropland"
-vcmax25_warmingco2_siteinfo$ecosystem[vcmax25_warmingco2_siteinfo$SiteID=="MississippiCE"] <- "cropland"
-vcmax25_warmingco2_siteinfo$ecosystem[vcmax25_warmingco2_siteinfo$SiteID=="WheatCE"] <- "cropland"
-vcmax25_warmingco2_siteinfo$ecosystem[vcmax25_warmingco2_siteinfo$SiteID=="ChinaMiniFACE"] <- "cropland"
-vcmax25_warmingco2_siteinfo$ecosystem[vcmax25_warmingco2_siteinfo$SiteID=="GrassOTC"] <- "grassland"
-vcmax25_warmingco2_siteinfo$ecosystem[vcmax25_warmingco2_siteinfo$SiteID=="PHACE"] <- "grassland"
-vcmax25_warmingco2_siteinfo$ecosystem[vcmax25_warmingco2_siteinfo$SiteID=="FescueCE"] <- "grassland"
-vcmax25_warmingco2_siteinfo$ecosystem[vcmax25_warmingco2_siteinfo$SiteID=="NZFACE"] <- "grassland"
-vcmax25_warmingco2_siteinfo$ecosystem[vcmax25_warmingco2_siteinfo$SiteID=="NevadaFACE"] <- "grassland"
-vcmax25_warmingco2_siteinfo$ecosystem[vcmax25_warmingco2_siteinfo$SiteID=="BioCON"] <- "grassland"
-vcmax25_warmingco2_siteinfo$ecosystem[vcmax25_warmingco2_siteinfo$SiteID=="PhalarisGC"] <- "grassland"
-vcmax25_warmingco2_siteinfo$ecosystem[vcmax25_warmingco2_siteinfo$SiteID=="SwissFACE"] <- "grassland"
-
 #now, combine with Kevin:
 smith_all_plotmean <- subset(vcmax25_warmingco2_siteinfo,exp_nam!="NZFACE" &exp_nam!="BioCON" & exp_nam!="ChinaRiceFACE" & exp_nam!="DukeFACE" & exp_nam!="EUROPOPFACE" & exp_nam!="NevadaFACE" & exp_nam!="SwissFACE")
 
@@ -106,13 +94,67 @@ smith_all_plotmean$exp[smith_all_plotmean$exp=="Headley"] <- "headley_qp_c";
 smith_all_plotmean$exp[smith_all_plotmean$exp=="Viesalm"] <- "vielsalm_c"
 
 smith_all_plotmean$exp <- tolower(smith_all_plotmean$exp)
-smith_all_plotmean2 <- unique(smith_all_plotmean[,c("exp","exp_nam","co2_a","co2_e")])
+smith_all_plotmean$site <- smith_all_plotmean$exp_nam
+
+smith_all_plotmean2 <- unique(smith_all_plotmean[,c("exp","site","co2_a","co2_e")])
 
 plot2 <- merge(plot1,smith_all_plotmean2,by=c("exp"),all.x=TRUE)
-plot2$site[is.na(plot2$site)==TRUE] <- plot2$exp_nam[is.na(plot2$site)==TRUE]
+# 1 points added - perhaps because there are more than 1 co2_a and co2_e 
+unique(plot2[,c("site.y","co2_a.y","co2_e.y")])
+#it is WheatCE
 
+#fill info
+plot2$site.x[is.na(plot2$site.x)==TRUE] <- plot2$site.y[is.na(plot2$site.x)==TRUE]
 plot2$co2_a.x[is.na(plot2$co2_a.x)==TRUE] <- plot2$co2_a.y[is.na(plot2$co2_a.x)==TRUE] 
 plot2$co2_e.x[is.na(plot2$co2_e.x)==TRUE] <- plot2$co2_e.y[is.na(plot2$co2_e.x)==TRUE]
+
+plot2 <- dplyr::select(plot2, -c("site.y", "co2_a.y","co2_e.y"))
+
+plot2$site.x[is.na(plot2$site.x)==TRUE] <- plot2$exp[is.na(plot2$site.x)==TRUE]
+
+#add fertilization point's co2 a and e
+#input co2 change
+plot2$co2_a.x[plot2$lon==-84.04] <- 360
+plot2$co2_e.x[plot2$lon==-84.04] <- 560 
+#https://pubmed.ncbi.nlm.nih.gov/11960756/
+plot2$co2_a.x[plot2$lon==20.00] <- 350
+plot2$co2_e.x[plot2$lon==20.00] <- 700
+#https://www.researchgate.net/publication/242459413_Nutrient_and_genotypic_effects_on_CO2-responsiveness_Photosynthetic_regulation_in_Leucadendron_species_of_a_nutrient-poor_environment
+plot2$co2_a.x[plot2$lon==89.14] <-360
+plot2$co2_e.x[plot2$lon==89.14] <- 720 
+
+#clarify type-name
+plot2 <- subset(plot2,condition!="highN") # remove replicated sites
+plot2$condition[plot2$condition=="lowN"] <- "CO2 at high and low N"
+
+plot2$condition[plot2$type_name=="No_fertilization"] <- "CO2 at N and without N"
+
+#remove below since it is repliacted site-name in table (already mentioned in above)
+plot2 <- subset(plot2,condition!="Fertilization")
+
+
+#add temperature info (just added in co2 info)
+plot2$co2_a.x[plot2$site=="Black Spruce, ON, Canada"] <- 22
+plot2$co2_e.x[plot2$site=="Black Spruce, ON, Canada"] <- 30
+
+plot2$co2_a.x[plot2$site=="Corymbia calophylla provs, AU-NSW"] <- 26
+plot2$co2_e.x[plot2$site=="Corymbia calophylla provs, AU-NSW"] <- 32
+
+plot2$co2_a.x[plot2$site=="Eucalyptus tereticornis provs AU-NSW"] <- 18
+plot2$co2_e.x[plot2$site=="Eucalyptus tereticornis provs AU-NSW"] <- 28.5
+
+plot2$co2_a.x[plot2$site=="Eucalyptus globulus, AU-NSW"] <- 20
+plot2$co2_e.x[plot2$site=="Eucalyptus globulus, AU-NSW"] <- 23
+
+plot2$co2_a.x[plot2$site=="Eucalyptus parramattensis, AU-NSW"] <- 15
+plot2$co2_e.x[plot2$site=="Eucalyptus parramattensis, AU-NSW"] <- 18
+
+plot2$co2_a.x[plot2$site=="Eucalyptus tereticornis, AU-NSW"] <- 14.4
+plot2$co2_e.x[plot2$site=="Eucalyptus tereticornis, AU-NSW"] <- 22.6
+
+#all warming, light, N fertilization plots are in forest or shrub. Fully checked.
+plot2$ecosystem[is.na(plot2$ecosystem)==T] <- "Forest"
+
 
 csvfile <- paste("~/data/gcme/MS_data/table_org_site.csv")
 write.csv(plot2, csvfile, row.names = TRUE)
